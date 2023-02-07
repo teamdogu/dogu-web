1c58e5323f9ca9006cdb15833910c6083dfe742d
teamdogu <dogu_ai@proton.me>
2023-02-07 21:03:26 +0800

    mr clay tell jokes

--- src/Joke.jsx
+++ src/Joke.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 
 import dogusit from "./assets/dogu-sit.webp";
+import dogudad from "./assets/dogudad.png";
+
 import loadingimg from "./assets/loading.gif";
 
 function Joke() {
@@ -10,38 +12,47 @@ function Joke() {
     setup: "GM, Dogu don't know where you live, but GM!",
     punchline: "",
   });
+  const [isDogu, setIsDogu] = useState(true);
   const [dadJoke, setDadJoke] = useState("");
   const [loading, setLoading] = useState(false);
   const [line, setLine] = useState("");
   const baseURL = "https://official-joke-api.appspot.com/jokes/random";
+
   const getJokes = () => {
     setLoading(true);
-    axios.get(baseURL).then((res) => {
-      const setup = res.data.setup;
-      const punchline = res.data.punchline;
-      setLine("");
-      setJokes({
-        setup,
-        punchline,
+    if (isDogu) {
+      axios.get(baseURL).then((res) => {
+        const setup = res.data.setup;
+        const punchline = res.data.punchline;
+        setLine("");
+        setJokes({
+          setup,
+          punchline,
+        });
+        setLoading(false);
       });
-      setLoading(false);
-    });
-  };
-  const addPunchline = () => {
-    setLine(jokes.punchline);
+    } else {
+      const axiosConfig = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      };
+      const baseURL = "https://icanhazdadjoke.com/";
+      axios.get(baseURL, axiosConfig).then((res) => {
+        const setup = res.data.joke;
+
+        setLine("");
+        setJokes({
+          setup,
+        });
+        setLoading(false);
+      });
+    }
   };
 
-  const getDadJoke = () => {
-    const axiosConfig = {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    };
-    const baseURL = "https://icanhazdadjoke.com/";
-    axios.get(baseURL, axiosConfig).then((res) => {
-      console.log(res.data.joke);
-    });
+  const addPunchline = () => {
+    setLine(jokes.punchline);
   };
 
   return (
@@ -55,16 +66,46 @@ function Joke() {
       </Link>
       <div className="md:grid grid-cols-2 gap-7 items-center md:w-3/5 h-4/5 my-0 mx-auto">
         <div className=" md:w-4/5 my-0 mx-auto">
-          <img className="w-3/5 my-0 mx-auto" src={dogusit} alt="" />
+          <img
+            className="w-3/5 my-0 mx-auto"
+            src={isDogu ? dogusit : dogudad}
+            alt=""
+          />
           <div className="flex justify-between">
-            <button className="bg-amber-500 py-2 px-5">Dogu</button>
-            <button className="bg-amber-500 py-2 px-5">Mr. Clay</button>
+            <button
+              onClick={() => {
+                setIsDogu(true);
+                setJokes({
+                  setup: "GM, Dogu don't know where you live, but GM!",
+                  punchline: "",
+                });
+              }}
+              className={`bg-amber-500 py-2 px-5 ${
+                isDogu ? "bg-amber-500" : "bg-white"
+              }`}
+            >
+              Dogu
+            </button>
+            <button
+              onClick={() => {
+                setIsDogu(false);
+                setJokes({
+                  setup: "Hi Kid, Good Morning, always morning in here",
+                  punchline: "",
+                });
+              }}
+              className={`bg-amber-500 py-2 px-5 ${
+                isDogu ? "bg-white" : "bg-amber-500"
+              }`}
+            >
+              Mr. Clay
+            </button>
           </div>
         </div>
 
         <div>
           <div className="flex items-end mb-2">
-            <p className="text-2xl">Dogu:</p>{" "}
+            <p className="text-xl mt-2">{isDogu ? "Dogu:" : "Mr. Clay:"}</p>{" "}
             {loading && <img className="w-1/6" src={loadingimg} />}
           </div>
 
@@ -75,17 +116,19 @@ function Joke() {
 
           <button
             onClick={getJokes}
-            className="mr-3 mt-10 border-solid border-2 border-indigo-400 justify-center py-2 px-6 rounded-full hover:bg-indigo-100"
+            className={`mr-3 mt-10 border-solid border-2 border-indigo-400 justify-center py-2 px-6 rounded-full hover:bg-indigo-100`}
           >
-            Dogu, Tell Me A Joke
+            {isDogu ? "Dogu, Tell Me A Joke" : "Mr. Clay, any joke?"}
           </button>
+          {isDogu && (
+            <button
+              onClick={addPunchline}
+              className="mt-5 border-solid border-2 border-indigo-400 justify-center py-2 px-6 rounded-full hover:bg-indigo-100"
+            >
+              Punchline
+            </button>
+          )}
 
-          <button
-            onClick={addPunchline}
-            className="mt-10 border-solid border-2 border-indigo-400 justify-center py-2 px-6 rounded-full hover:bg-indigo-100"
-          >
-            Punchline
-          </button>
           {/* <button
             onClick={getDadJoke}
             className="mt-10 border-solid border-2 border-indigo-400 justify-center py-2 px-6 rounded-full hover:bg-indigo-100"
